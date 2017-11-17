ab434d8e690c1dcb442deef0d6ff781de26db5bc
Jarrett Melnick <Jarrett.c.Melnick@live.mercer.edu>
2017-11-17 11:28:30 -0500

    FINISHED Collatz.java and Collat_rec.java

---
+++ Java/Recursive/Collatz_rec.java
@@ -0,0 +1,70 @@
+public class Collatz_rec{
+	public static void main(String [] args){
+		//long iterate = Long.MAX_VALUE;
+		long iterate = 13;
+		number[] result = new number[10]; 
+
+		for(int i = 0; i < 10; i++){
+			result[i] = new number();
+		}
+
+		for(long i = iterate; i > 1; i--){
+			long curCollatz = collatzConjecture(i,0);
+			if(curCollatz > result[9].collatzVal){
+				number num = new number(i, curCollatz);
+				result[9] = num;
+				result = sorter(result);
+			}
+		}
+		result = sorter(result);
+		for(int i = 0; i < 10; i++){
+			System.out.println(i+1 + " is " + result[i].identity + 
+				" and has a collatz conjecture of " + result[i].collatzVal);
+		}
+	}
+	private static long collatzConjecture(long num, long collatz){
+		if(num <= 1){
+			return collatz;
+		}
+		else{
+			return ((num % 2 == 1) ? (collatzConjecture(3*num+1,collatz+1)): (collatzConjecture(num/2,collatz+1)));
+		}
+	}
+	private static number[] sorter(number num[]){
+		boolean sorted = false;
+		while(!sorted){
+			sorted = true;
+
+			for(int i = 1; i < 10 ; i++){
+				if(num[i].collatzVal > num[i-1].collatzVal){
+					number temp = num[i-1];
+					num[i-1] = num[i];
+					num[i] = temp;
+					sorted = false;
+				}
+				else if(num[i].collatzVal == num[i-1].collatzVal && num[i].identity > num[i-1].identity){
+					number temp = num[i-1];
+					num[i-1] = num[i];
+					num[i] = temp;
+					sorted = false;
+				}
+			}
+		}
+		return num;
+	}
+}
+class number{
+	public long identity = 0;
+	public long collatzVal = 0;
+	public number(){
+		identity = 0;
+		collatzVal = 0;
+	}
+	public number(long ident, long colVal){
+		identity = ident;
+		collatzVal = colVal;
+	}
+	public long getColatzVal(){
+		return collatzVal;
+	}
+}
