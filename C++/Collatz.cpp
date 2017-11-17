1da5496e2a9d3cea9c728567e322e4eb67fa095b
Jarrett Melnick <Jarrett.c.Melnick@live.mercer.edu>
2017-11-17 14:36:07 -0500

    Added comments to Collatz.cpp :

--- C++/Collatz.cpp
+++ C++/Collatz.cpp
@@ -1,7 +1,10 @@
+//Author: Jarrett Melnick
+//Program to find the numbers with the largest Collatz sequence in a range.
 #include <iostream>
 
 using namespace std;
 
+//Main struct to keep track of numbers and thier Collatz sequence length.
 struct number{
 	long identity = 0;
 	long collatzVal = 0;
@@ -11,9 +14,12 @@ long collatzConjecture(long num);
 void sorter(number num[10]);
 
 int main(){
+
+	//Change this number to change the range. It should be the largest nubmer considered.
 	long iterate = 13;
 	number result[10];
 
+	//Runs the collatz conjecture on each number in the given range.
 	for(long i = iterate; i > 1; i--){
 		long curCollatz = collatzConjecture(i);
 		if(curCollatz > result[9].collatzVal){
@@ -28,6 +34,7 @@ int main(){
 	}
 }
 long collatzConjecture(long num){
+	//Function to perform a collatz conjecture on a given value.
 	long counter = 0;
 	while(num > 1){
 		num = ((num % 2 == 1) ? (3*num+1): (num/2));
@@ -36,6 +43,8 @@ long collatzConjecture(long num){
 	return counter;
 }
 void sorter(number num[]){
+	//Function to sort the array of Numbers first by size of collatz sequence and then by 
+	//identity.
 	bool sorted = false;
 	while(!sorted){
 		sorted = true;
