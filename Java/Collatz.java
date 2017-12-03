196d938afb997d5c7d61e1672cfe50958e126746
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-12-03 01:08:05 -0500

    Fixed bug in Java programs that would cause the program to attempt to calculate a Collatz sequnce for zero as well as improving the sort.

--- Java/Collatz.java
+++ Java/Collatz.java
@@ -2,24 +2,29 @@
 //Program to find the top 10 Collatz sequences in a given range
 public class Collatz{
 	public static void main(String [] args){
-		//long iterate = Long.MAX_VALUE;
-		long iterate = 5000000000;			//Max value to iterate
+		//Change to modify the behavior of the program.
+		long iterate = 5000000000L;			//Max value to iterate
+
 		number[] result = new number[10]; 
 
 		for(int i = 0; i < 10; i++){
 			result[i] = new number();
 		}
-
-		for(long i = iterate; i > 1; i--){
+        
+        System.out.println("Calculating...");
+		for(long i = 2; i <= iterate; i++){
 			long curCollatz = collatzConjecture(i);
-			if(curCollatz > result[9].collatzVal){
+            //System.out.println(i);
+			if((curCollatz > result[9].collatzVal) && !(hasCollatzNumber(result, curCollatz))){
 				number num = new number(i, curCollatz);
 				result[9] = num;
+				//System.out.println("SORTING...");
 				result = sorter(result);
 			}
 		}
 		//Prints the top 10 numbers with the largest collatz sequence.
 		result = sorter(result);
+        System.out.println("Building Results...");
 		for(int i = 0; i < 10; i++){
 			System.out.println(i+1 + " is " + result[i].identity + 
 				" and has a collatz conjecture of " + result[i].collatzVal);
@@ -29,10 +34,19 @@ public class Collatz{
 		//Calculates the length of the Collatz sequence for a given number.
 		long counter = 0;
 		while(val > 1){
-			val = ((val % 2) == 1) ? (3*val+1): (val/2);
+			//System.out.println(val);
+			val = (val % 2 == 1) ? (3*val+1) : (val/2);
 			counter++;
 		}
 		return counter;
+	}
+	private static boolean hasCollatzNumber(number num[], long val){
+		//Determines if the given array num has a number with a Collatz sequence of length val.
+		for(int i = 0; i < 10; i++){
+			if(num[i].collatzVal == val)
+				return true;
+		}
+		return false;
 	}
 	private static number[] sorter(number num[]){
 		//Sorts the array by the length of the collatz sequence
@@ -47,12 +61,12 @@ public class Collatz{
 					num[i] = temp;
 					sorted = false;
 				}
-				else if(num[i].collatzVal == num[i-1].collatzVal){
+				/*else if(num[i].collatzVal == num[i-1].collatzVal){
 					number temp = num[i-1];
 					num[i-1] = num[i];
 					num[i] = temp;
 					sorted = false;
-				}
+				}*/
 			}
 		}
 		return num;
