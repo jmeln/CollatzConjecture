585fbb06f86d6b0f55f8c4b6890b2f03e03432c0
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-12-04 02:56:31 -0500

    Working versions of Collatz.rb and Collatz_rec.rb

--- Ruby/Collatz.rb
+++ Ruby/Collatz.rb
@@ -1,19 +1,23 @@
 #!/usr/bin/env ruby
 #Author: Jarrett Melnick
 #Program to print numbers with the smallest Collatz values in a given range.
-def Collatz(num = 1, numArray)
+class CollatzNumber
+	def initialize(identity, sequence)
+		@identity, @sequence = identity, sequence
+	end
+	def GetIdentity
+		@identity
+	end
+	def GetSequence
+		@sequence
+	end
+end
+def Collatz(num = 1)
 	#Function to perform the collatz sequence on a given number
-	#numArray has the collatz values of the previous iterations to improve runtime.
 	seq = 0
-
 	until num < 2 		#Essentially while(!true). Will run until the condition is met
-		if numArray[num] != nil		
-			seq = seq + numArray[num]
-			break
-		else
-			num = (num.to_s(2)[-1] == '0') ? (num/2) : (3*num+1)
-			seq += 1
-		end
+		num = (num.to_s(2)[-1] == '0') ? (num/2) : (3*num+1)
+		seq += 1
 	end
 	return seq
 end
@@ -22,23 +26,55 @@ def GetLargest(numArray)
 	max = 0
 	numArray.each_index{|i|
 		if numArray[i] != nil
-			if numArray[max] < numArray[i]
+			if numArray[max].GetSequence < numArray[i].GetSequence
 				max = i
 			end
 		end
 	}
 	return max
 end
-
-iterate = 13			#The largest number to be considered. Changes range of numbers iterated.
-
-nums = [0, 1, 1]
-for i in 3..iterate do 
-	nums[i] = Collatz(i, nums)
+def GetSmallest(numArray)
+	#Returns the index of the number wiht the smallest collatz sequence
+	min = 0;
+	numArray.each_index{|i|
+		if numArray[i] != nil
+			if numArray[min].GetSequence > numArray[i].GetSequence
+				min = i
+			end
+		end
+	}
+	return min
 end
+def HasCollatzNumber(numArray, val)
+	#Checks if the array contains val. Returns true if val is found.
+	numArray.each_index{|i|
+		if numArray[i] != nil
+			if numArray[i].GetSequence == val
+				return true
+			end
+		end
+	}
+	return false
+end
+
+print "Please provide an endpoint: "
+iterate = gets.to_i			#The largest number to be considered. Changes range of numbers iterated.
+
+nums = Array.new(10, CollatzNumber.new(0,0))
+
+puts "Calculating Collatz Values..."
+for i in 2..iterate
+	smallest = GetSmallest(nums)
+	curCollatz = Collatz(i)
+	if((curCollatz > nums[smallest].GetSequence) and !(HasCollatzNumber(nums, curCollatz)))
+		num = CollatzNumber.new(i, curCollatz)
+		nums[smallest] = num
+	end
+end
+puts "Building Table..."
 #Prints top 10 numbers with the largest collatz sequence.
-for i in 1..10 do
+for i in 1..10
 	largest = GetLargest(nums)
-	puts "#{largest} has a collaz sequence of #{nums[largest]}"
-	nums[largest] = nil
-end		
+	puts "#{nums[largest].GetIdentity} has a collaz sequence of #{nums[largest].GetSequence}"
+	nums[largest] = CollatzNumber.new(0,0)
+end
--- Ruby/Recursive/Collatz_rec.rb
+++ Ruby/Recursive/Collatz_rec.rb
@@ -1,41 +1,78 @@
 #!/usr/bin/env ruby
 #Author: Jarrett Melnick
-#Program to print numbers with the smallest Collatz values.
-def Collatz(num = 1, numArray, seq)
-	#Function to perform the collatz sequence on a given number
-	#numArray has the collatz values of the previous iterations to improve runtime.
-	if numArray[num] != nil
-		return seq + numArray[num]
-	elsif num < 2
-		return seq +1
-	else
-		seq += 1
-		return (num.to_s(2)[-1] == '0') ? Collatz((num/2), numArray, seq) : Collatz((3*num+1), numArray, seq)
+#Program to print numbers with the smallest Collatz values in a given range.
+class CollatzNumber
+	def initialize(identity, sequence)
+		@identity, @sequence = identity, sequence
 	end
+	def GetIdentity
+		@identity
+	end
+	def GetSequence
+		@sequence
+	end
+end
+def Collatz(num = 1, seq = 0)
+	#Function to perform the collatz sequence on a given number
+	if num < 2
+		return seq
+	end
+	return (num.to_s(2)[-1] == '0') ? Collatz((num/2), (seq+1)) : Collatz((3*num+1), (seq+1))
 end
 def GetLargest(numArray)
 	#Returns the index of the number with the largest collatz sequence
 	max = 0
 	numArray.each_index{|i|
 		if numArray[i] != nil
-			if numArray[max] < numArray[i]
+			if numArray[max].GetSequence < numArray[i].GetSequence
 				max = i
 			end
 		end
 	}
 	return max
 end
-
-iterate = 13			#The largest value to check.
-	
-nums = [0, 1, 1]
-for i in 3..iterate do 
-	nums[i] = Collatz(i, nums, 0)
+def GetSmallest(numArray)
+	#Returns the index of the number wiht the smallest collatz sequence
+	min = 0;
+	numArray.each_index{|i|
+		if numArray[i] != nil
+			if numArray[min].GetSequence > numArray[i].GetSequence
+				min = i
+			end
+		end
+	}
+	return min
+end
+def HasCollatzNumber(numArray, val)
+	#Checks if the array contains val. Returns true if val is found.
+	numArray.each_index{|i|
+		if numArray[i] != nil
+			if numArray[i].GetSequence == val
+				return true
+			end
+		end
+	}
+	return false
 end
 
-#Prints the top 10 numbers with the largest Collatz sequence
-for i in 1..10 do
+print "Please provide an endpoint: "
+iterate = gets.to_i			#The largest number to be considered. Changes range of numbers iterated.
+
+nums = Array.new(10, CollatzNumber.new(0,0))
+
+puts "Calculating Collatz Values..."
+for i in 2..iterate
+	smallest = GetSmallest(nums)
+	curCollatz = Collatz(i)
+	if((curCollatz > nums[smallest].GetSequence) and !(HasCollatzNumber(nums, curCollatz)))
+		num = CollatzNumber.new(i, curCollatz)
+		nums[smallest] = num
+	end
+end
+puts "Building Table..."
+#Prints top 10 numbers with the largest collatz sequence.
+for i in 1..10
 	largest = GetLargest(nums)
-	puts "#{largest} has a collaz sequence of #{nums[largest]}"
-	nums[largest] = nil
-end		
+	puts "#{nums[largest].GetIdentity} has a collaz sequence of #{nums[largest].GetSequence}"
+	nums[largest] = CollatzNumber.new(0,0)
+end
