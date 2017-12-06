4f43a8db8971c789f3c9dfa38aec9c861e47b720
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-12-05 21:12:11 -0500

    Moving to cobra

---
+++ Ada/Test.adb
@@ -0,0 +1,8 @@
+--Author: Jarrett Melnick
+--Program to calculate the top 10 numbers with the largest Collatz sequences
+with Text_IO;
+
+procedure Test is
+begin
+	Text_IO.Put_Line("Hello World in Ada");
+end Test;
