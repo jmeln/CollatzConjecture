a2190b80164e925b34e8a63b7a4049f783a2de5f
Jarrett Melnick <jarrett.c.melnick@live.mercer.edu>
2017-12-11 19:49:41 -0500

    Finished Ada version Collatz.adb

---
+++ Ada/Collatz.adb
@@ -0,0 +1,105 @@
+--Author: Jarrett Melnick
+--Program to calculate the top 10 numbers with the largest Collatz sequences
+with Text_IO, Ada.Integer_Text_IO;
+use Text_IO, Ada.Integer_Text_IO;
+
+procedure Collatz is
+
+function sequence(num: in out Integer) return Integer is
+steps : Integer := 1;
+begin
+	while num > 2
+	loop
+		if num mod 2 = 1 then
+			num := 3*num+1;
+		else
+			num := num / 2;
+		end if;
+		steps := steps + 1;
+	end loop;
+	return steps;
+end sequence;
+--Variable Declarations
+iterate: Integer := 15; 	--Change this to change max
+smallest: Integer := 0;
+largest: Integer := 0;
+curCollatz: Integer := 0;
+hasCollatz: Integer:= 0;
+inp :Integer:=0;
+
+--Array Declarations
+ident1: array(0 .. 9) of Integer;
+ident2: array(0 .. 9) of Integer;
+seq1: array(0 .. 9) of Integer;
+seq2: array(0 .. 9) of Integer;
+begin
+	--Set arrays equal to zero
+	for i in 0 ..9 loop
+		ident1(i) := 0;
+		ident2(i) := 0;
+		seq1(i) := 0;
+		seq2(i) := 0;
+	end loop;
+--Find the top 10 numbers with the largest sequences
+	For_Loop:
+	for i in Integer range 2 .. iterate loop
+		smallest := 0;
+		hasCollatz:= 0;
+		inp := i;
+		curCollatz := sequence(inp);
+		For_Loop2:
+		for j in Integer range 0 .. 9 loop
+			if seq1(smallest) > seq1(j) then
+				smallest := j;
+			end if;
+			if seq1(j) = curCollatz then
+			hasCollatz := 1;
+			end if;
+		end loop For_Loop2;
+		if seq1(smallest) < curCollatz and hasCollatz = 0 then
+			seq1(smallest) := curCollatz;
+			seq2(smallest) := curCollatz;
+			ident1(smallest) := i;
+			ident2(smallest) := i;
+		end if;
+	end loop For_Loop;
+
+	--Print Output
+	put_line("SORTED BY SEQUENCE SIZE:");
+	For_Loop4:
+	for i in Integer range 0 .. 9 loop
+		largest := 0;
+		For_Loop5:
+		for j in Integer range 0 .. 9 loop
+			if seq1(largest) < seq1(j) then
+				largest := j;
+			end if;
+		end loop For_Loop5;
+		put(Integer'Image(ident1(largest)));
+		put (" has a Collatz sequence of length ");
+		put_line(Integer'Image(seq1(largest)));
+		seq1(largest) := 0;
+		ident1(largest) := 0;
+
+	end loop For_Loop4;
+	
+	put_line("SORTED BY INTEGER SIZE:");
+	For_Loop6:
+	for i in Integer range 0 .. 9 loop
+		largest := 0;
+		For_Loop7:
+		for k in Integer range 0 .. 9 loop
+			if ident2(largest) < ident2(k)then
+				largest := k;
+			end if;
+		end loop For_Loop7;
+
+	put(Integer'Image(ident2(largest)));
+	put(" has a Collatz sequence of length ");
+	put_line(Integer'Image(seq2(largest)));
+	seq2(largest) := 0;
+	ident2(largest) := 0;
+	end loop For_Loop6;
+
+--	put_line(Long_Integer'Image(Collatz(z)));
+end Collatz;
